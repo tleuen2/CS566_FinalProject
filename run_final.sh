1d096e267cbee4e08c8d2ab0a79b7a92c6f89639
Thomas Leuenberger <tleuen2@uic.edu>
2019-05-05 14:12:23 -0500

    space in pbs script

--- run_final.sh
+++ run_final.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-qsub finalprojectrun.pbs -l nodes=1:ppn=1
-qsub finalprojectrun.pbs -l nodes=1:ppn=2
-qsub finalprojectrun.pbs -l nodes=1:ppn=4
-qsub finalprojectrun.pbs -l nodes=1:ppn=8
-qsub finalprojectrun.pbs -l nodes=1:ppn=16
+qsub QE_Config_Run.pbs -l nodes=1:ppn=1
+qsub QE_Config_Run.pbs -l nodes=1:ppn=2
+qsub QE_Config_Run.pbs -l nodes=1:ppn=4
+qsub QE_Config_Run.pbs -l nodes=1:ppn=8
+qsub QE_Config_Run.pbs -l nodes=1:ppn=16

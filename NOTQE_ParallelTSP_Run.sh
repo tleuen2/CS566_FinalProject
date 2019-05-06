9af113ccc53f0713eb4db2e99770437301e751e0
Thomas Leuenberger <tleuen2@uic.edu>
2019-05-05 20:42:47 -0500

    copy to extreme

--- NOTQE_ParallelTSP_Run.sh
+++ NOTQE_ParallelTSP_Run.sh
@@ -6,36 +6,10 @@
 #qsub RUN.pbs -l nodes=1:ppn=8
 #qsub RUN.pbs -l nodes=1:ppn=16
 
-qsub NOTQE28INPUT.pbs -F "1 1" -l nodes=1:ppn=1
-qsub NOTQE28INPUT.pbs -F "1 1" -l nodes=1:ppn=2
-qsub NOTQE28INPUT.pbs -F "1 1" -l nodes=1:ppn=4
-qsub NOTQE28INPUT.pbs -F "1 1" -l nodes=1:ppn=8
-qsub NOTQE28INPUT.pbs -F "1 1" -l nodes=1:ppn=16
 
 
-qsub NOTQE28INPUT.pbs -F "2 1" -l nodes=1:ppn=1
-qsub NOTQE28INPUT.pbs -F "2 1" -l nodes=1:ppn=2
-qsub NOTQE28INPUT.pbs -F "2 1" -l nodes=1:ppn=4
-qsub NOTQE28INPUT.pbs -F "2 1" -l nodes=1:ppn=8
-qsub NOTQE28INPUT.pbs -F "2 1" -l nodes=1:ppn=16
-
-
-qsub NOTQE28INPUT.pbs -F "3 1" -l nodes=1:ppn=1
-qsub NOTQE28INPUT.pbs -F "3 1" -l nodes=1:ppn=2
-qsub NOTQE28INPUT.pbs -F "3 1" -l nodes=1:ppn=4
-qsub NOTQE28INPUT.pbs -F "3 1" -l nodes=1:ppn=8
-qsub NOTQE28INPUT.pbs -F "3 1" -l nodes=1:ppn=16
-
-
-qsub NOTQE28INPUT.pbs -F "4 1" -l nodes=1:ppn=1
-qsub NOTQE28INPUT.pbs -F "4 1" -l nodes=1:ppn=2
-qsub NOTQE28INPUT.pbs -F "4 1" -l nodes=1:ppn=4
-qsub NOTQE28INPUT.pbs -F "4 1" -l nodes=1:ppn=8
-qsub NOTQE28INPUT.pbs -F "4 1" -l nodes=1:ppn=16
-
-
-qsub NOTQE28INPUT.pbs -F "5 1" -l nodes=1:ppn=1
-qsub NOTQE28INPUT.pbs -F "5 1" -l nodes=1:ppn=2
-qsub NOTQE28INPUT.pbs -F "5 1" -l nodes=1:ppn=4
-qsub NOTQE28INPUT.pbs -F "5 1" -l nodes=1:ppn=8
-qsub NOTQE28INPUT.pbs -F "5 1" -l nodes=1:ppn=16
+qsub NOTQE28INPUT.pbs -l nodes=1:ppn=1
+qsub NOTQE28INPUT.pbs -l nodes=1:ppn=2
+qsub NOTQE28INPUT.pbs -l nodes=1:ppn=4
+qsub NOTQE28INPUT.pbs -l nodes=1:ppn=8
+qsub NOTQE28INPUT.pbs -l nodes=1:ppn=16

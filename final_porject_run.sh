629a2d640fd70aaf13ef81c083c043f401fdb896
Thomas Leuenberger <tleuen2@uic.edu>
2019-05-10 00:58:25 -0500

    copy to extreme

--- final_porject_run.sh
+++ final_porject_run.sh
@@ -100,7 +100,7 @@ qsub FinalProject28INPUT.pbs -F "4 1 1" -l nodes=1:ppn=16
 #sleep 10
 qsub FinalProject28INPUT.pbs -F "4 1 2" -l nodes=2:ppn=8
 #sleep 10
-qsub FinalProject28INPUT.pbs -F "4 1 2" -l nodes=2:ppn=16
+#qsub FinalProject28INPUT.pbs -F "4 1 2" -l nodes=2:ppn=16
 #sleep 10
 #sleep 10
 #qsub FinalProject28INPUT.pbs -F "5 1 2" -l nodes=2:ppn=1
@@ -209,7 +209,7 @@ qsub FinalProject28INPUT.pbs -F "4 1 2" -l nodes=2:ppn=16
 #sleep 10
 qsub FinalProject28INPUT.pbs -F "4 1 4" -l nodes=4:ppn=4
 #sleep 10
-qsub FinalProject28INPUT.pbs -F "4 1 4" -l nodes=4:ppn=8
+#qsub FinalProject28INPUT.pbs -F "4 1 4" -l nodes=4:ppn=8
 #sleep 10
 #qsub FinalProject28INPUT.pbs -F "4 1 4" -l nodes=4:ppn=16
 #sleep 10
@@ -428,7 +428,7 @@ qsub FinalProject28INPUT.pbs -F "4 1 4" -l nodes=4:ppn=8
 #sleep 10
 qsub FinalProject28INPUT.pbs -F "4 1 8" -l nodes=8:ppn=2
 #sleep 10
-qsub FinalProject28INPUT.pbs -F "4 1 8" -l nodes=8:ppn=4
+#qsub FinalProject28INPUT.pbs -F "4 1 8" -l nodes=8:ppn=4
 #sleep 10
 #qsub FinalProject28INPUT.pbs -F "4 1 8" -l nodes=8:ppn=8
 #sleep 10

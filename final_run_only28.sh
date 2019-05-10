582151ccae62c89215378e1cba02998543418cb5
Thomas Leuenberger <tleuen2@uic.edu>
2019-05-10 16:47:01 -0500

    copy to extreme

--- final_run_only28.sh
+++ final_run_only28.sh
@@ -13,51 +13,51 @@
 
 #sleep 10
 
-#qsub INPUT28.pbs -F "2 1 1" -l nodes=1:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "2 1 1" -l nodes=1:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "2 1 1" -l nodes=1:ppn=4
-#sleep 10
-#qsub INPUT28.pbs -F "2 1 1" -l nodes=1:ppn=8
-#sleep 10
-#qsub INPUT28.pbs -F "2 1 1" -l nodes=1:ppn=16
-#sleep 10
-#sleep 10
-
-#qsub INPUT28.pbs -F "3 1 1" -l nodes=1:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "3 1 1" -l nodes=1:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "3 1 1" -l nodes=1:ppn=4
-#sleep 10
-#qsub INPUT28.pbs -F "3 1 1" -l nodes=1:ppn=8
-#sleep 10
-#qsub INPUT28.pbs -F "3 1 1" -l nodes=1:ppn=16
-#sleep 10
+qsub INPUT28.pbs -F "2 1 1" -l nodes=1:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "2 1 1" -l nodes=1:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "2 1 1" -l nodes=1:ppn=4
+sleep 10
+qsub INPUT28.pbs -F "2 1 1" -l nodes=1:ppn=8
+sleep 10
+qsub INPUT28.pbs -F "2 1 1" -l nodes=1:ppn=16
+sleep 10
 #sleep 10
 
-#qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=4
-#sleep 10
-#qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=8
-#sleep 10
+qsub INPUT28.pbs -F "3 1 1" -l nodes=1:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "3 1 1" -l nodes=1:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "3 1 1" -l nodes=1:ppn=4
+sleep 10
+qsub INPUT28.pbs -F "3 1 1" -l nodes=1:ppn=8
+sleep 10
+qsub INPUT28.pbs -F "3 1 1" -l nodes=1:ppn=16
+sleep 10
+sleep 10
+
+qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=4
+sleep 10
+qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=8
+sleep 10
 qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=16
+sleep 10
 #sleep 10
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 1" -l nodes=1:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 1" -l nodes=1:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 1" -l nodes=1:ppn=4
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 1" -l nodes=1:ppn=8
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 1" -l nodes=1:ppn=16
-#sleep 10
+qsub INPUT28.pbs -F "5 1 1" -l nodes=1:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "5 1 1" -l nodes=1:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "5 1 1" -l nodes=1:ppn=4
+sleep 10
+qsub INPUT28.pbs -F "5 1 1" -l nodes=1:ppn=8
+sleep 10
+qsub INPUT28.pbs -F "5 1 1" -l nodes=1:ppn=16
+sleep 10
 #sleep 10
 #qsub INPUT28.pbs -F "1 1 2" -l nodes=2:ppn=1
 #sleep 10
@@ -70,46 +70,46 @@ qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=16
 #qsub INPUT28.pbs -F "1 1 2" -l nodes=2:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "2 1 2" -l nodes=2:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "2 1 2" -l nodes=2:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "2 1 2" -l nodes=2:ppn=4
-#sleep 10
-#qsub INPUT28.pbs -F "2 1 2" -l nodes=2:ppn=8
+qsub INPUT28.pbs -F "2 1 2" -l nodes=2:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "2 1 2" -l nodes=2:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "2 1 2" -l nodes=2:ppn=4
+sleep 10
+qsub INPUT28.pbs -F "2 1 2" -l nodes=2:ppn=8
 #sleep 10
 #qsub INPUT28.pbs -F "2 1 2" -l nodes=2:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "3 1 2" -l nodes=2:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "3 1 2" -l nodes=2:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "3 1 2" -l nodes=2:ppn=4
-#sleep 10
-#qsub INPUT28.pbs -F "3 1 2" -l nodes=2:ppn=8
-#sleep 10
+qsub INPUT28.pbs -F "3 1 2" -l nodes=2:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "3 1 2" -l nodes=2:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "3 1 2" -l nodes=2:ppn=4
+sleep 10
+qsub INPUT28.pbs -F "3 1 2" -l nodes=2:ppn=8
+sleep 10
 #qsub INPUT28.pbs -F "3 1 2" -l nodes=2:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "4 1 2" -l nodes=2:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "4 1 2" -l nodes=2:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "4 1 2" -l nodes=2:ppn=4
-#sleep 10
-#qsub INPUT28.pbs -F "4 1 2" -l nodes=2:ppn=8
+qsub INPUT28.pbs -F "4 1 2" -l nodes=2:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "4 1 2" -l nodes=2:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "4 1 2" -l nodes=2:ppn=4
+sleep 10
+qsub INPUT28.pbs -F "4 1 2" -l nodes=2:ppn=8
 #sleep 10
 #qsub INPUT28.pbs -F "4 1 2" -l nodes=2:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "5 1 2" -l nodes=2:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 2" -l nodes=2:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 2" -l nodes=2:ppn=4
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 2" -l nodes=2:ppn=8
+qsub INPUT28.pbs -F "5 1 2" -l nodes=2:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "5 1 2" -l nodes=2:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "5 1 2" -l nodes=2:ppn=4
+sleep 10
+qsub INPUT28.pbs -F "5 1 2" -l nodes=2:ppn=8
 #sleep 10
 #qsub INPUT28.pbs -F "5 1 2" -l nodes=2:ppn=16
 #sleep 10
@@ -181,45 +181,45 @@ qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=16
 #qsub INPUT28.pbs -F "1 1 4" -l nodes=4:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "2 1 4" -l nodes=4:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "2 1 4" -l nodes=4:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "2 1 4" -l nodes=4:ppn=4
-#sleep 10
+qsub INPUT28.pbs -F "2 1 4" -l nodes=4:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "2 1 4" -l nodes=4:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "2 1 4" -l nodes=4:ppn=4
+sleep 10
 #qsub INPUT28.pbs -F "2 1 4" -l nodes=4:ppn=8
 #sleep 10
 #qsub INPUT28.pbs -F "2 1 4" -l nodes=4:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "3 1 4" -l nodes=4:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "3 1 4" -l nodes=4:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "3 1 4" -l nodes=4:ppn=4
-#sleep 10
+qsub INPUT28.pbs -F "3 1 4" -l nodes=4:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "3 1 4" -l nodes=4:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "3 1 4" -l nodes=4:ppn=4
+sleep 10
 #qsub INPUT28.pbs -F "3 1 4" -l nodes=4:ppn=8
 #sleep 10
 #qsub INPUT28.pbs -F "3 1 4" -l nodes=4:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "4 1 4" -l nodes=4:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "4 1 4" -l nodes=4:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "4 1 4" -l nodes=4:ppn=4
-#sleep 10
+qsub INPUT28.pbs -F "4 1 4" -l nodes=4:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "4 1 4" -l nodes=4:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "4 1 4" -l nodes=4:ppn=4
+sleep 10
 #qsub INPUT28.pbs -F "4 1 4" -l nodes=4:ppn=8
 #sleep 10
 #qsub INPUT28.pbs -F "4 1 4" -l nodes=4:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "5 1 4" -l nodes=4:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 4" -l nodes=4:ppn=2
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 4" -l nodes=4:ppn=4
-#sleep 10
+qsub INPUT28.pbs -F "5 1 4" -l nodes=4:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "5 1 4" -l nodes=4:ppn=2
+sleep 10
+qsub INPUT28.pbs -F "5 1 4" -l nodes=4:ppn=4
+sleep 10
 #qsub INPUT28.pbs -F "5 1 4" -l nodes=4:ppn=8
 #sleep 10
 #qsub INPUT28.pbs -F "5 1 4" -l nodes=4:ppn=16
@@ -402,10 +402,10 @@ qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=16
 #qsub INPUT28.pbs -F "1 1 8" -l nodes=8:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "2 1 8" -l nodes=8:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "2 1 8" -l nodes=8:ppn=2
-#sleep 10
+qsub INPUT28.pbs -F "2 1 8" -l nodes=8:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "2 1 8" -l nodes=8:ppn=2
+sleep 10
 #qsub INPUT28.pbs -F "2 1 8" -l nodes=8:ppn=4
 #sleep 10
 #qsub INPUT28.pbs -F "2 1 8" -l nodes=8:ppn=8
@@ -413,10 +413,10 @@ qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=16
 #qsub INPUT28.pbs -F "2 1 8" -l nodes=8:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "3 1 8" -l nodes=8:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "3 1 8" -l nodes=8:ppn=2
-#sleep 10
+qsub INPUT28.pbs -F "3 1 8" -l nodes=8:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "3 1 8" -l nodes=8:ppn=2
+sleep 10
 #qsub INPUT28.pbs -F "3 1 8" -l nodes=8:ppn=4
 #sleep 10
 #qsub INPUT28.pbs -F "3 1 8" -l nodes=8:ppn=8
@@ -424,10 +424,10 @@ qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=16
 #qsub INPUT28.pbs -F "3 1 8" -l nodes=8:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "4 1 8" -l nodes=8:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "4 1 8" -l nodes=8:ppn=2
-#sleep 10
+qsub INPUT28.pbs -F "4 1 8" -l nodes=8:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "4 1 8" -l nodes=8:ppn=2
+sleep 10
 #qsub INPUT28.pbs -F "4 1 8" -l nodes=8:ppn=4
 #sleep 10
 #qsub INPUT28.pbs -F "4 1 8" -l nodes=8:ppn=8
@@ -435,10 +435,10 @@ qsub INPUT28.pbs -F "4 1 1" -l nodes=1:ppn=16
 #qsub INPUT28.pbs -F "4 1 8" -l nodes=8:ppn=16
 #sleep 10
 #sleep 10
-#qsub INPUT28.pbs -F "5 1 8" -l nodes=8:ppn=1
-#sleep 10
-#qsub INPUT28.pbs -F "5 1 8" -l nodes=8:ppn=2
-#sleep 10
+qsub INPUT28.pbs -F "5 1 8" -l nodes=8:ppn=1
+sleep 10
+qsub INPUT28.pbs -F "5 1 8" -l nodes=8:ppn=2
+sleep 10
 #qsub INPUT28.pbs -F "5 1 8" -l nodes=8:ppn=4
 #sleep 10
 #qsub INPUT28.pbs -F "5 1 8" -l nodes=8:ppn=8
